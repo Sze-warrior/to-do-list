f0b228fdad5cc6f4092755f02422b081fa26f884
Michael Szeto <szetomj@gmail.com>
2016-02-26 21:13:16 +1300

    managed to add a delete button and remove all the <li> tags from the screen

--- js/to-do-list-functions.js
+++ js/to-do-list-functions.js
@@ -6,12 +6,15 @@ $(document).ready(function(){
 	$(addButton).click(function(){
 
     	var toAdd = $('input[name=ListItem]').val();    
-		$(ToDolist).append('<li>' + toAdd + '</li>');
+		$(ToDolist).append('<li class="list-item">' + toAdd + '<button class="delete">Delete</button>' + '</li>');
+
+	$('.delete').click(function(e){
+
+		$(e.currentTarget).hide();
+		$('.list-item').hide();
+
 	});
 
-	$('.delete').click(function(){
-
-		$('li').hide();
 
 	});
 });
