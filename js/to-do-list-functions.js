b4cc61028e75beb532a5794d83b1bc069b908a75
Michael Szeto <szetomj@gmail.com>
2016-05-16 22:37:42 +1200

    stopping users from entering in blank tasks

--- js/to-do-list-functions.js
+++ js/to-do-list-functions.js
@@ -5,13 +5,17 @@ $(document).ready(function(){
 
 	$(addButton).click(function(){
     	var taskAdded = $('input[name=ListItem]').val();
-	
+
+    	if(taskAdded.length < 1){
+    		console.log("Enter something");
+    	}else{
 		$(taskList).append('<li>' + taskAdded + '<button class="delete">Delete</button>' + '</li>');
 		$(taskList).children('li').addClass('list-item');
-	});
+    	}
 
-	$('.delete').click(function(e){
+    		$('.delete').click(function(e){
 		$(e.currentTarget).parent('.list-item').remove();
 	});
 
+	});
 });
