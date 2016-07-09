550b4a50ea372b176a0c151c9e827dc6cc770bd4
Michael Szeto <szetomj@hotmail.co.nz>
2016-07-10 00:22:08 +1200

    began the error state

--- js/to-do-list-functions.js
+++ js/to-do-list-functions.js
@@ -2,15 +2,17 @@ $(document).ready(function(){
 
 	var taskList = $('.task-list');
 	var addButton = $('.btn-add');
+	var alert = $('.alert');
 
 	$(addButton).click(function(){
     	var taskAdded = $('input[name=ListItem]').val();
 
     	if(taskAdded.length < 1){
-    		console.log("Enter something");
+    		$(alert).show();
     	}else{
 		$(taskList).append('<li>' + taskAdded + '<button class="delete">Delete</button>' + '</li>');
 		$(taskList).children('li').addClass('list-item');
+		$(alert).hide();
     	}
 
     		$('.delete').click(function(e){
