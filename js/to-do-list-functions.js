487edf4be3e783d8ffab1e3157ff66154b3fe5f7
Michael Szeto <szetomj@hotmail.co.nz>
2017-02-05 00:12:18 +1300

    Fixed styling andadded action to clear form when submitting task

--- js/to-do-list-functions.js
+++ js/to-do-list-functions.js
@@ -8,15 +8,12 @@ $(document).ready(function(){
     	var taskAdded = $('input[name=ListItem]').val();
 
     	if(taskAdded.length < 1){
-    	
     		$(alert).show();
-    	
     	}else{
-		
-		$(taskList).append('<li>' + taskAdded + '<button class="delete">Delete</button>' + '</li>');
-		$(taskList).children('li').addClass('list-item');
-		$(alert).hide();
-    	
+			$(taskList).append('<li>' + taskAdded + '<div class="delete">Delete</div>' + '</li>');
+			$(taskList).children('li').addClass('list-item');
+			$(alert).hide();
+	    	$('input[name=ListItem]').val("");
     	}
 
     	$('.delete').click(function(e){
